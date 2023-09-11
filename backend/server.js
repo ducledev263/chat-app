73dec6d2d5c11e89b491624645e8cb2b1a9a2923
Darkie Lee <ducle0906.ulis@gmail.com>
2023-09-11 23:05:26 +0700

    add some api call for express

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
 const app = require("express")();
 const dotenv = require("dotenv");
-// const res = require("express/lib/response");
+const { chats } = require("./data/data");
+
+let cors = require("cors");
+app.use(cors());
+
 dotenv.config();
 
 app.get("/", (req, res) => {
     res.send("hi")
+})
+
+app.get("/api/chat", (req, res) => {
+    res.send(chats)
+})
+
+app.get("/api/chat/:id", (req, res) => {
+    const singleChat = chats.find(chat => chat._id === req.params.id);
+    res.send(singleChat)
 })
 
 const PORT = process.env.PORT || 5000
