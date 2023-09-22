490c540ee3b3f8a5ea9bcb0a6ba7bf35798cbe4f
Darkie Lee <ducle0906.ulis@gmail.com>
2023-09-22 17:40:09 +0700

    modify the file

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const { protect } = require("../middlewares/authMiddleware");
-const { sendMessage } = require("../controllers/messageControllers");
+const { sendMessage, allMessages } = require("../controllers/messageControllers");
 const router = express.Router();
 
 router.route("/").post(protect, sendMessage);
-// router.route("/:chatId").get(protect, AllMessages);
+router.route("/:chatId").get(protect, allMessages);
 
 module.exports = router;
