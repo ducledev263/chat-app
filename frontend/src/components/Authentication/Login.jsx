3c078b82fea698c3c23d5260d1df1c87c35cf10a
Darkie Lee <ducle0906.ulis@gmail.com>
2023-09-25 18:44:12 +0700

    test headers

--- frontend/src/components/Authentication/Login.jsx
+++ frontend/src/components/Authentication/Login.jsx
@@ -34,6 +34,8 @@ const Login = () => {
             const config = {
                 headers: {
                     "Content-type": "application/json",
+                    'Accept': 'application/json',
+                    'Origin': 'https://chat-app-frontend-cngi.onrender.com/'
                 }
             }
             const { data } = await axios.post(`https://chat-app-backend-zzgd.onrender.com/api/user/login`, { email, password }, config);
