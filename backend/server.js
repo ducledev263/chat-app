334be2371c04b477954ce38c21455cc87ce918ee
Darkie Lee <ducle0906.ulis@gmail.com>
2023-09-25 17:41:27 +0700

    test header

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://chat-app-frontend-cngi.onrender.com/");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next()
 });
