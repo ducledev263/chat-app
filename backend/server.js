93f60e3ec60ff2d8edd61c49941ccd4c35f314ba
Darkie Lee <ducle0906.ulis@gmail.com>
2023-09-25 17:02:38 +0700

    test server

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000
 
-const server = createServer();
+const server = createServer(app);
 
 server.listen(PORT, console.log(`Server starts on port ${PORT}`));
 
